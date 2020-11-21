971a9e9e9e3c704248afe5cda632c080f72b2b01
JøranL <jli054@uit.no>
2020-11-21 20:01:50 +0100

    updates

--- config.def.h
+++ config.def.h
@@ -1,33 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar                       = 1;            /* -b  option; if 0, dmenu appears at bottom     */
-static int centered                     = 1;            /* -c option; centers dmenu on screen */
-static int min_width                    = 700;          /* minimum width when centered */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
+static int topbar               = 1;                      /* -b  option; if 0, dmenu appears at bottom */
+static int fuzzy                = 0;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int center               = 1;                      /* -c  option; if 0, dmenu won't be centered on the screen */
+static int min_width            = 700;                 /* minimum width when centered */
+static const char *fonts[]      =
+{
 	"monospace:size=14"
 };
-static const char *prompt               = NULL;         /* -p  option; prompt to the left of input field */
-static const char col_bg[]              = "#ebdbb2";    // White 
-static const char col_fg[]              = "#282828";    // Dark
-static const char col_sel[]             = "#fb4934";    // Red
-static const char col_tag[]             = "#928374";    // Gray
-static const char *colors[SchemeLast][2] = {
-	/*                    fg          bg       */
-	[SchemeNorm]    = { col_fg,     col_bg  },
-	[SchemeSel]     = { col_tag,    col_sel },
-	[SchemeOut]     = { col_fg,     col_bg  },
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char col_bg[]     = "#2c2e34";    // Dark
+static const char col_fg[]     = "#e2e2e3";    // White
+static const char col_sel[]    = "#9ed072";    // Green
+static const char col_high[]   = "#fc5d7c";    // Red
+static const char *colors[][2] =
+{
+	/*               fg           bg       */
+	[SchemeNorm] = { col_fg,    col_bg  },
+	[SchemeSel]  = { col_fg,    col_sel },
+	[SchemeOut]  = { col_fg,    col_bg  },
+	[SchemeSelHighlight]  = { col_bg, col_sel  },
+	[SchemeNormHighlight] = { col_fg, col_bg  },
 };
-/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 7;
 static unsigned int columns    = 3;
+static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+static unsigned int maxhist    = 15;
+static int histnodup           = 1;	/* if 0, record repeated histories */
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
 static const char worddelimiters[] = " ";
 
-/* Size of the window border */
-static const unsigned int border_width = 1;
+static unsigned int border_width = 1;
